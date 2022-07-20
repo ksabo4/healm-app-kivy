61b32fa0e4d489bc7ff6f5f1e9cbb294a0cb5422
Marco Mambelli <marcom@fnal.gov>
2022-07-20 16:57:19 -0500

    Populated project form template

---
+++ setup.py
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2022 We
+# SPDX-License-Identifier: Apache-2.0
+
+# comment
+
+from setuptools import setup
+
+setup(
+    name="healm-app",
+    version="0.0.1",
+    packages=["healm"],
+    install_requires=[
+        "numpy",
+    ],
+)
