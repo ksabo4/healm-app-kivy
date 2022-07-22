2112d5fbdd03ff56b2a32864f2e3911cd19af803
russeltjahjadi <rtjahj2@uic.edu>
2022-07-22 11:08:04 -0500

    Some added files for tutorials

--- src/kivvy-test/main.py
+++ src/kivvy-test/main.py
@@ -3,20 +3,10 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 
-'''
+
 #how you can see the layout box from the python file
 class BoxLayoutExample(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        #making buttons:
-        b1 = Button(text = "button A")
-        b2 = Button(text = "button B")
-
-        #adding the widget in the layout
-        self.add_widget(b1)
-        self.add_widget(b2)
-'''
+    pass
 
 #main interface
 class MainWidget(Widget):
@@ -28,3 +18,22 @@ class TheLabApp(App):
 TheLabApp().run()
 
 #how do we comment in the kv file?
+
+
+'''
+class BoxLayoutExample(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+        self.orientation = "horizontal" #you can also make it vertical (but that would change the orientation of the buttons
+
+        #making buttons:
+        b1 = Button(text = "button A")
+        b2 = Button(text = "button B")
+
+        #adding the widget in the layout
+        self.add_widget(b1)
+        self.add_widget(b2)
+
+'''
